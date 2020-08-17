faf5aa12d27c9c5c1d7ae87a4ad8184123e409f5
moisutsu <moisutsu@gmail.com>
2020-08-17 13:17:27 +0900

    Add using get macro sample

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
 
+#[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
-}
-
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
+            .service(index)
     })
     .bind("127.0.0.1:8088")?
     .run()

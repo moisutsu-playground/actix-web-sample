4b826b5454b3e514d7d109e30f3cdd2b562e1f04
moisutsu <moisutsu@gmail.com>
2020-08-17 13:20:44 +0900

    Update format

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -7,11 +7,8 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(index)
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8088")?
+        .run()
+        .await
 }

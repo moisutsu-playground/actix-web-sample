61d1f949aa23d42155dac44902827f949cdcf9fd
moisutsu <moisutsu@gmail.com>
2020-08-17 13:42:29 +0900

    Add scope sample

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
 
-#[get("/")]
+#[get("/world")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -9,7 +9,7 @@ async fn index() -> impl Responder {
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().service(index));
+    let mut server = HttpServer::new(|| App::new().service(web::scope("/hello").service(index)));
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l)?
